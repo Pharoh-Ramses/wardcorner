2f623339aa530ef043dadce72449cf9cb83615fc
Ramses Angles <ramses.angles@hey.com>
2025-11-18 13:26:38 -0600

    fixed a ref issue

--- src/components/content/SacramentProgramSection.tsx
+++ src/components/content/SacramentProgramSection.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Link from 'next/link'
 import type { SacramentProgram } from '@/payload-types'
 import { formatDate } from '@/lib/utils/formatters'
 import SacramentProgramCard from './SacramentProgramCard'
